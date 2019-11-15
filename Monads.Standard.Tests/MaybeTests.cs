527170d217aee35a00ee9e2bf6d015dfe93fbbef
Chris Thompson <Darthruneis@hotmail.com>
2019-11-14 23:49:55 -0500

    Enhance Maybe with monad methods.

--- Monads.Standard.Tests/MaybeTests.cs
+++ Monads.Standard.Tests/MaybeTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using FakeItEasy;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -61,6 +62,129 @@ namespace Monads.Tests
 
             first.HasNoValue.Should().BeTrue();
             action.Should().Throw<InvalidOperationException>();
+        }
+
+        [Test]
+        public void MapShouldNotCallFunctionWhenHasNoValue()
+        {
+            var none = Maybe<int>.Empty();
+
+            var mapFunction = A.Fake<Func<int, Maybe<int>>>();
+
+            none.Map(mapFunction);
+
+            A.CallTo(() => mapFunction.Invoke(A<int>._)).MustNotHaveHappened();
+        }
+        [Test]
+        public void MapShouldCallFunctionWhenHasValue()
+        {
+            var none = Maybe<int>.Create(5);
+
+            var mapFunction = A.Fake<Func<int, Maybe<int>>>();
+
+            none.Map(mapFunction);
+
+            A.CallTo(() => mapFunction.Invoke(5)).MustHaveHappened();
+        }
+
+        [Test]
+        public void MapShouldReturnEmptyMaybeWhenNoValue()
+        {
+            var none = Maybe<int>.Empty();
+
+            var result = none.Map<int>(x => 5);
+
+            result.HasNoValue.Should().BeTrue();
+        }
+
+        [Test]
+        public void CoalesceShouldReturnValueWhenHasValue()
+        {
+            var value = Maybe<int>.Create(5);
+
+            var result = value.Coalesce(3);
+
+            result.Should().Be(5);
+        }
+
+        [Test]
+        public void CoalesceShouldReturnDefaultValueWhenHasNoValue()
+        {
+            var none = Maybe<int>.Empty();
+
+            var result = none.Coalesce(3);
+
+            result.Should().Be(3);
+        }
+
+
+        [Test]
+        public void ApplyShouldNotCallFunctionWhenHasNoValue()
+        {
+            var none = Maybe<int>.Empty();
+
+            var action = A.Fake<Action<int>>();
+
+            none.Apply(action);
+
+            A.CallTo(() => action.Invoke(A<int>._)).MustNotHaveHappened();
+        }
+
+        [Test]
+        public void ApplyShouldCallFunctionWhenHasValue()
+        {
+            var none = Maybe<int>.Create(5);
+
+            var action = A.Fake<Action<int>>();
+
+            none.Apply(action);
+
+            A.CallTo(() => action.Invoke(5)).MustHaveHappened();
+        }
+
+        [Test]
+        public void ChainingShouldWork()
+        {
+            Maybe<int> init = 5;
+
+            var mapper1 = A.Fake<Func<int, Maybe<string>>>();
+
+            A.CallTo(() => mapper1.Invoke(5))
+             .Returns("5");
+
+            var mapper2 = A.Fake<Func<string, Maybe<int>>>();
+
+            A.CallTo(() => mapper2.Invoke("5"))
+             .Returns(5);
+
+            var mapper3 = A.Fake<Func<int, Maybe<string>>>();
+            A.CallTo(() => mapper3.Invoke(5))
+             .Returns(Maybe<string>.Empty());
+
+            var mapper4 = A.Fake<Func<string, Maybe<int>>>();
+            A.CallTo(() => mapper4.Invoke("5"))
+             .Returns(5);
+
+            var apply = A.Fake<Action<int>>();
+
+            init.Map(mapper1).Map(mapper2).Map(mapper3)
+                .Map(mapper4)
+                .Apply(apply);
+
+            A.CallTo(() => mapper1.Invoke(5))
+             .MustHaveHappened();
+
+            A.CallTo(() => mapper2.Invoke("5"))
+             .MustHaveHappened();
+
+            A.CallTo(() => mapper3.Invoke(5))
+             .MustHaveHappened();
+
+            A.CallTo(() => mapper4.Invoke(A<string>._))
+             .MustNotHaveHappened();
+
+            A.CallTo(() => apply.Invoke(A<int>._))
+             .MustNotHaveHappened();
         }
     }
 }
--- Monads.Standard/Maybe.cs
+++ Monads.Standard/Maybe.cs
@@ -6,8 +6,7 @@ namespace Monads
     /// A wrapper for an object that might be null.
     /// </summary>
     /// <typeparam name="T">The type of object that may be contained inside the wrapper.</typeparam>
-    public class Maybe<T>
-        where T: class
+    public class Maybe <T>
     {
         private T _value;
 
@@ -15,11 +14,11 @@ namespace Monads
         /// The value of the maybe.
         /// </summary>
         /// <exception cref="InvalidOperationException">If the maybe does not have a value.</exception>
-        public T Value 
+        public T Value
         {
             get
             {
-                if(HasNoValue)
+                if (HasNoValue)
                     throw new InvalidOperationException("Can not access the value of an empty Maybe.");
 
                 return _value;
@@ -47,35 +46,55 @@ namespace Monads
             HasValue = true;
             Value = value;
         }
-        
+
+        /// <summary>
+        /// Calls the specified <paramref name="f"></paramref>
+        /// </summary>
+        /// <typeparam name="TResult">The type of result for the operation that will be mapped.</typeparam>
+        /// <param name="f">The function to call on the value when it is present.WW
+        /// </param>
+        /// <returns></returns>
+        public Maybe<TResult> Map <TResult>(Func<T, Maybe<TResult>> f) => !HasValue ? Maybe<TResult>.Empty() : f(Value);
+
+        /// <summary>
+        /// Retrieves the value that is stored, or the default value
+        /// when no value is available.
+        /// </summary>
+        /// <param name="defaultValue">The default value to use.</param>
+        /// <returns></returns>
+        public T Coalesce(T defaultValue) => HasValue ? Value : defaultValue;
+
+        /// <summary>
+        /// Applies the provided <paramref name="f"/> to the <see href="Value"/>
+        /// when the value is present.
+        /// </summary>
+        /// <param name="f">The function to be applied if there is a value stored.</param>
+        /// <returns></returns>
+        public void Apply(Action<T> f)
+        {
+            if (HasValue) f(Value);
+        }
+
         /// <summary>
         /// Creates a maybe with a value.
         /// </summary>
         /// <param name="value">The value for the maybe.</param>
         /// <returns></returns>
-        public static Maybe<T> Create(T value)
-        {
-            return ReferenceEquals(null, value)
-                       ? new Maybe<T>()
-                       : new Maybe<T>(value);
-        }
+        public static Maybe<T> Create(T value) =>
+            ReferenceEquals(null, value)
+                ? new Maybe<T>()
+                : new Maybe<T>(value);
 
         /// <summary>
         /// Creates a maybe without a value.
         /// </summary>
         /// <returns></returns>
-        public static Maybe<T> Empty()
-        {
-            return new Maybe<T>();
-        }
+        public static Maybe<T> Empty() => new Maybe<T>();
 
         /// <summary>
         /// Allows all values to be cast as a Maybe, for example from a call to FirstOrDefault().
         /// </summary>
         /// <param name="value"></param>
-        public static implicit operator Maybe<T> (T value)
-        {
-            return Create(value);
-        }
+        public static implicit operator Maybe<T> (T value) => Create(value);
     }
 }
